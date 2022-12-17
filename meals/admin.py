1c9515dbc3bac1ff5f5485c0dc74d0aa5870ecea
Andy <andy@freilandkiwis.de>
2022-12-17 21:03:32 +0100

    Zuletzt-Spalte im Admin

--- meals/admin.py
+++ meals/admin.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
+from django.db.models import OuterRef, Subquery
 
 from meals.models import Day, Meal, Plan, Week
 
 
 @admin.register(Meal)
 class MealAdmin(admin.ModelAdmin):
-    list_display = ['name', 'created']
+    list_display = ['name', 'created', 'latest']
     ordering = ['-created']
     search_fields = ['name']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        latest_plan_subquery = Plan.data.filter(
+            meal=OuterRef('pk')).order_by('-created').values('day__date')[:1]
+        qs = qs.annotate(latest=Subquery(latest_plan_subquery))
+        return qs
+
+    def latest(self, obj):
+        return obj.latest
+    latest.short_description = "Zuletzt"
+    latest.admin_order_field = 'latest'
 
 
 class DayInlineAdmin(admin.TabularInline):
