60112450eea6f525a756b1b57454d87e9f1e21fa
Andy <andy@freilandkiwis.de>
2024-06-04 14:50:30 +0200

    neue Adresse

--- settings/common.py
+++ settings/common.py
@@ -28,11 +28,11 @@ SITE_ID = 1
 DEBUG = False
 ALLOWED_HOSTS = [
     "*",
-    "https://menue.mama-filmcatering.de",
+    "https://menue.luises-familienbande.de",
 ]
 CORS_ALLOWED_ORIGINS = CSRF_TRUSTED_ORIGINS = CORS_ORIGIN_WHITELIST = [
     "http://localhost:8080",
-    "https://menue.mama-filmcatering.de",
+    "https://menue.luises-familienbande.de",
 ]
 CORS_ALLOW_CREDENTIALS = True  # allow cookies
 
